cd427fe37f398978432f7cd0514ef778ac96b7da
ricardo <ricardogp1993@gmail.com>
2018-11-27 15:00:25 -0200

    Separation of the synchronization of the executors and beginning of the implementation of the loggers.

---
+++ src/main/SynchronizationCognitiveArchitecture.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import actionSelection.SynchronizationMethods;
+import br.unicamp.cst.core.entities.Codelet;
+import synchronization.MyLock;
+import br.unicamp.cst.core.entities.MemoryObject;
+import java.util.Map;
+
+/**
+ *
+ * @author rgpolizeli
+ */
+public class SynchronizationCognitiveArchitecture {
+    
+    public SynchronizationCognitiveArchitecture() {
+    }
+    
+    public static void synchronizeExecutor(String executorName, Codelet codelet, MemoryObject executorHandleMO, MemoryObject synchronizerMO){
+        SynchronizationMethods.getSynchronizerLock().lock();
+        Map<String, MyLock> myLocks = (Map<String, MyLock>) synchronizerMO.getI();
+        try{
+            executorHandleMO.setI(Boolean.FALSE);
+            codelet.setLoop(Boolean.FALSE);
+            SynchronizationMethods.destroyLock(executorName, myLocks);
+        } finally{
+            SynchronizationMethods.getSynchronizerLock().unlock();
+        }
+    }
+    
+}
